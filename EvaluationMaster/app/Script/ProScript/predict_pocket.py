f8b1a936fa250ea4a43b06345447adfd9644204c
shenzheyuan2020 <73147896+shenzheyuan2020@users.noreply.github.com>
2024-05-21 14:42:32 +0800

    Add files via upload

--- EvaluationMaster/app/Script/ProScript/predict_pocket.py
+++ EvaluationMaster/app/Script/ProScript/predict_pocket.py
@@ -1,6 +1,3 @@
-#python predict_pocket.py /path/to/Support_software/DeepPocket/ /path/to/input/folder /path/to/coordinate.xlsx GPU_number
-#example: python /home/hoo/Install/Evaluation_X/EvaluationMaster/app/Script/ProScript/predict_pocket.py /home/hoo/Install/Evaluation_X/Support_software/DeepPocket/ /home/hoo/Install/Evaluation_X/pdb_pocket_predict_test/ /home/hoo/Install/Evaluation_X/pdb_pocket_predict_test/coordinate.xlsx 0
-
 from Bio.PDB import PDBParser, PDBIO, Select
 import Bio
 import pandas as pd
@@ -22,12 +19,12 @@ from skimage.segmentation import clear_border
 from skimage.measure import label
 import struct
 
-#Load Deeppocket function
+# Load Deeppocket function
 deep_pocket_path = sys.argv[1]
 sys.path.append(deep_pocket_path)
 from clean_pdb import clean_pdb
 from get_centers import get_centers
-from types_and_gninatyper import gninatype,create_types
+from types_and_gninatyper import gninatype, create_types
 from model import Model
 from rank_pockets import test_model
 from unet import Unet
@@ -38,7 +35,7 @@ import gc
 def set_gpu(gpu_num):
     if torch.cuda.is_available():
         try:
-            gpu_num = int(gpu_num)
+            gpu_num = int(gpu_num) - 1  # user input (1-based) to match PyTorch's 0-based GPU indexing
             torch.cuda.set_device(gpu_num)
             print(f"Using GPU: {gpu_num}")
         except Exception as e:
@@ -58,10 +55,10 @@ def get_model_gmaker_eprovider(test_types, batch_size, model, checkpoint, dims=N
     else:
         gmaker = molgrid.GridMaker(dimension=dims)
     return model, gmaker, eptest_large
-                
-                
+
+
 def process_pdb_files(input_folder, class_checkpoint_path, seg_checkpoint_path, rank=1, upsample='None', num_classes=1, threshold=0.5, mask_dist=3.5):
-    pdb_files = [f for f in os.listdir(input_folder) if f.endswith('.pdb')] 
+    pdb_files = [f for f in os.listdir(input_folder) if f.endswith('.pdb')]
 
     for pdb_file in pdb_files:
         try:
@@ -88,32 +85,35 @@ def process_pdb_files(input_folder, class_checkpoint_path, seg_checkpoint_path, 
             # Rank pockets
             with torch.no_grad():
                 class_model = Model()
-                # class_checkpoint = torch.load(class_checkpoint_path)
                 class_checkpoint = torch.load(class_checkpoint_path, map_location=torch.device('cpu'))
 
                 with open(class_types, 'r') as file:
                     types_lines = file.readlines()
                 batch_size = min(len(types_lines), 50)
                 class_model, class_gmaker, class_eptest = get_model_gmaker_eprovider(class_types, batch_size, class_model, class_checkpoint)
-                
+
                 # Get ranking and probabilities
                 class_labels, class_probs = test_model(class_model, class_eptest, class_gmaker, batch_size)
                 sorted_zipped_lists = sorted(zip(class_probs[:len(types_lines)], types_lines), reverse=True)
                 ranked_types = [element for _, element in sorted_zipped_lists]
                 confidence_types = [str(element) for element, _ in sorted_zipped_lists]
 
-            # Save rankings and probabilities to files
-            with open(seg_types, 'w') as fout:
-                fout.writelines(ranked_types)
-            with open(probs_types_file, 'w') as fout:
-                fout.writelines(f'{confidence}\n' for confidence in confidence_types)
+            # Only save the top-ranked type
+            if ranked_types:
+                top_ranked_type = ranked_types[0]
+                top_confidence_type = confidence_types[0]
+
+                with open(seg_types, 'w') as fout:
+                    fout.writelines([top_ranked_type])
+                with open(probs_types_file, 'w') as fout:
+                    fout.writelines([f'{top_confidence_type}\n'])
 
         except Exception as e:
             print(f"Error processing {pdb_file}: {e}")
 
         finally:
             # Clean up resources
-            del class_model, class_checkpoint 
+            del class_model, class_checkpoint
             gc.collect()
             torch.cuda.empty_cache()
 
@@ -131,15 +131,33 @@ def extract_coordinates(input_folder):
                         parts = line.strip().split()
                         if len(parts) == 4:
                             pocket_id, x, y, z = parts
+                            # Only add the first pocket found
                             data.append([f"{molecule_name}-{pocket_id}", x, y, z])
+                            break  # Exit after the first pocket is added
     return data
 
 
-def save_coordinates_to_excel(data, excel_file_path):
-    if not excel_file_path.endswith('.xlsx'):
-        raise ValueError("Excel file path must end with '.xlsx'")
+def save_coordinates(data, output_dir):
+    # Construct the full file path for both Excel and CSV
+    excel_file_path = os.path.join(output_dir, "coordinates.xlsx")
+    csv_file_path = os.path.join(output_dir, "coordinates.csv")
 
-    pd.DataFrame(data, columns=['molecule_pocket', 'x', 'y', 'z']).to_excel(excel_file_path, index=False)
+    # Create a DataFrame from the data
+    df = pd.DataFrame(data, columns=['mol_name', 'x', 'y', 'z'])
+
+    # Save the DataFrame to an Excel file
+    try:
+        df.to_excel(excel_file_path, index=False)
+        print(f"Data successfully saved to Excel: {excel_file_path}")
+    except Exception as e:
+        print(f"Failed to save data to Excel: {e}")
+
+    # Save the same DataFrame to a CSV file
+    try:
+        df.to_csv(csv_file_path, index=False)
+        print(f"Data successfully saved to CSV: {csv_file_path}")
+    except Exception as e:
+        print(f"Failed to save data to CSV: {e}")
 
 
 def main():
@@ -147,24 +165,24 @@ def main():
         print("Sufficient path parameters are required, including the GPU number.")
         sys.exit(1)
 
-    deep_pocket_path = sys.argv[1] 
+    deep_pocket_path = sys.argv[1]
     input_folder = sys.argv[2]
-    excel_file_path = sys.argv[3]
-    gpu_num = sys.argv[4]  
+    output_folder = sys.argv[3]
+    gpu_num = sys.argv[4]
 
     # Set GPU
     set_gpu(gpu_num)
 
     # Define checkpoint paths
     class_checkpoint_path = os.path.join(deep_pocket_path, 'first_model_fold1_best_test_auc_85001.pth.tar')
-    seg_checkpoint_path = os.path.join(deep_pocket_path, 'seg0_best_test_IOU_91.pth.tar')  
-    
+    seg_checkpoint_path = os.path.join(deep_pocket_path, 'seg0_best_test_IOU_91.pth.tar')
+
     # Process PDB files
-    process_pdb_files(input_folder, class_checkpoint_path, seg_checkpoint_path) 
+    process_pdb_files(input_folder, class_checkpoint_path, seg_checkpoint_path)
 
     # Extract and save coordinates
     coordinates = extract_coordinates(input_folder)
-    save_coordinates_to_excel(coordinates, excel_file_path)
+    save_coordinates(coordinates, output_folder)
 
 
 if __name__ == "__main__":
